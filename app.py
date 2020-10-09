c5ac32cb012420fc0404dd4ae34e59b0fd32256e
ilMatadore <restarter.dev@gmail.com>
2020-10-09 13:48:15 -0500

    adding server and routes

---
+++ app.py
@@ -0,0 +1,245 @@
+from flask import Flask, request, jsonify
+from flask_sqlalchemy import SQLAlchemy
+from flask_marshmallow import Marshmallow
+from flask_cors import CORS
+from flask_bcrypt import Bcrypt
+import os
+
+# Init app
+app = Flask(__name__)
+CORS(app)
+bcrypt = Bcrypt(app)
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+# Database
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + \
+    os.path.join(basedir, 'db.sqlite')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+# Init db
+db = SQLAlchemy(app)
+
+# Init ma
+ma = Marshmallow(app)
+
+# Product Class/Model
+
+
+class Product(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), unique=True)
+    description = db.Column(db.String(200))
+    price = db.Column(db.Float)
+    qty = db.Column(db.Integer)
+    img = db.Column(db.String)
+
+    def __init__(self, name, description, price, qty, img):
+        self.name = name
+        self.description = description
+        self.price = price
+        self.qty = qty
+        self.img = img
+
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100))
+    email = db.Column(db.String(100), unique=True, nullable=False)
+
+    def __init__(self, name, email):
+        self.name = name
+        self.email = email
+
+
+class Login(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    hash = db.Column(db.String(100), unique=True, nullable=False)
+    email = db.Column(db.String(100), unique=True, nullable=False)
+
+    def __init__(self, hash, email):
+        self.hash = hash
+        self.email = email
+# Product Schema
+
+
+class ProductSchema(ma.Schema):
+    class Meta:
+        fields = ('id', 'name', 'description', 'price', 'qty', 'img')
+
+
+class UserSchema(ma.Schema):
+    class Meta:
+        fields = ('id', 'name', 'email')
+
+
+class LoginSchema(ma.Schema):
+    class Meta:
+        fields = ('id', 'hash', 'email')
+
+
+# Init schema
+product_schema = ProductSchema()
+products_schema = ProductSchema(many=True)
+
+user_schema = UserSchema()
+users_schema = UserSchema(many=True)
+
+login_schema = LoginSchema()
+logins_schema = LoginSchema(many=True)
+
+# Create a Product
+
+
+@app.route('/product', methods=['POST'])
+def add_product():
+    name = request.json['name']
+    description = request.json['description']
+    price = request.json['price']
+    qty = request.json['qty']
+    img = request.json['img']
+
+    new_product = Product(name, description, price, qty, img)
+
+    db.session.add(new_product)
+    db.session.commit()
+
+    return product_schema.jsonify(new_product)
+
+# Create user
+
+
+@app.route('/register', methods=['POST'])
+def register():
+    name = request.json['name']
+    email = request.json['email']
+    pwd = request.json['pwd']
+
+    hash = bcrypt.generate_password_hash(pwd).decode('utf-8')
+
+    new_user = User(name, email)
+    new_login = Login(hash, email)
+
+    db.session.add(new_user)
+    db.session.add(new_login)
+    db.session.commit()
+
+    return user_schema.jsonify(new_user)
+
+
+# Login user
+@app.route('/login', methods=['POST'])
+def login():
+    email = request.json['email']
+    pwd = request.json['pwd']
+
+    userLogin = Login.query.filter_by(email=email).first()
+    if userLogin:
+        isValid = bcrypt.check_password_hash(userLogin.hash, pwd)
+        user = User.query.filter_by(email=email).first()
+    else:
+        return 'error logging in'
+
+    if isValid:
+        return user_schema.jsonify(user)
+    else:
+        return "error"
+
+# Get All Products
+
+
+@app.route('/shop', methods=['GET'])
+def get_products():
+    all_products = Product.query.all()
+    result = products_schema.dump(all_products)
+    return jsonify(result)
+
+# Get All Users
+
+
+@app.route('/login', methods=['GET'])
+def get_users():
+    all_users = User.query.all()
+    result = users_schema.dump(all_users)
+    return jsonify(result)
+
+# Get Single Products
+
+
+@app.route('/product/<id>', methods=['GET'])
+def get_product(id):
+    product = Product.query.get(id)
+    return product_schema.jsonify(product)
+
+# Get Single User
+
+
+@app.route('/profile/<id>', methods=['GET'])
+def get_user(id):
+    user = User.query.get(id)
+    return user_schema.jsonify(user)
+
+# Update a Product
+
+
+@app.route('/product/<id>', methods=['PUT'])
+def update_product(id):
+    product = Product.query.get(id)
+
+    name = request.json['name']
+    description = request.json['description']
+    price = request.json['price']
+    qty = request.json['qty']
+    img = request.json['img']
+
+    product.name = name
+    product.description = description
+    product.price = price
+    product.qty = qty
+    product.img = img
+
+    db.session.commit()
+
+    return product_schema.jsonify(product)
+
+# Update a User
+
+
+@app.route('/profile/<id>', methods=['PUT'])
+def update_profile(id):
+    user = User.query.get(id)
+
+    name = request.json['name']
+    user.name = name
+
+    db.session.commit()
+
+    return user_schema.jsonify(user)
+
+
+# Delete Product
+
+
+@app.route('/product/<id>', methods=['DELETE'])
+def delete_product(id):
+    product = Product.query.get(id)
+    db.session.delete(product)
+    db.session.commit()
+
+    return product_schema.jsonify(product)
+
+# Delete User
+
+
+@app.route('/profile/<id>', methods=['DELETE'])
+def delete_user(id):
+    user = User.query.get(id)
+    db.session.delete(user)
+    db.session.commit()
+
+    return user_schema.jsonify(user)
+
+
+# Run Server
+if __name__ == '__main__':
+    app.run(debug=True)
